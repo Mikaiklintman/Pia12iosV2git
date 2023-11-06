40c44bad6f62e5150bc2c22356279fa491203d61
Mikai <Mikaiklint@icloud.com>
2023-11-06 09:08:54 +0100

    Initial Commit

---
+++ Pia12iosV2git/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pia12iosV2git
+//
+//  Created by Makai Klintman on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pia12iosV2git/Pia12iosV2gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pia12iosV2gitApp.swift
+//  Pia12iosV2git
+//
+//  Created by Makai Klintman on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Pia12iosV2gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
